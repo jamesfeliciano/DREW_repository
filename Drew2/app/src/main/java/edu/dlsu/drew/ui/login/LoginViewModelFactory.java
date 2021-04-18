22762fb17c9a495beb1b0dbec98b15a450cc8070
jamesfeliciano <60353439+jamesfeliciano@users.noreply.github.com>
2021-04-18 17:45:27 +0800

    Add files via upload

---
+++ Drew2/app/src/main/java/edu/dlsu/drew/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package edu.dlsu.drew.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import edu.dlsu.drew.data.LoginDataSource;
+import edu.dlsu.drew.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
